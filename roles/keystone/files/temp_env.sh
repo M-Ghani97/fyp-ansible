5bc5d17e1fe4547441beacefe5bc72cc71bc3a3e
Usman Ghani <mghani828@gmail.com>
2020-08-23 22:25:56 +0500

    first commit

---
+++ roles/keystone/files/temp_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+$ export OS_USERNAME=admin
+$ export OS_PASSWORD=ADMIN_PASS
+$ export OS_PROJECT_NAME=admin
+$ export OS_USER_DOMAIN_NAME=Default
+$ export OS_PROJECT_DOMAIN_NAME=Default
+$ export OS_AUTH_URL=http://controller:5000/v3
+$ export OS_IDENTITY_API_VERSION=3
